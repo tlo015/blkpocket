29f4e90839c96c764cd2dea3dd94d7e4b8b3ffa1
ebartos <eric@frequency.com>
2018-06-30 03:19:44 -0700

    eric_branch

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,16 @@
 var express = require('express');
 var router = express.Router();
-
+var passport = require("../config/passport");
+var isAuthenticated = require("../config/middleware/isAuthenticated");
 var user_controller = require('../controllers/user_controller');
 
-router.get('/signin', user_controller.signin);
+router.get('/signup', user_controller.registrationPage);
 
-router.get('/account', user_controller.account);
+router.get('/sign-out', user_controller.signOutUser);
 
+router.post('/login', passport.authenticate("local"), user_controller.loginUser);
+
+router.post('/signup', user_controller.signUpUser);
 
 
 module.exports = router;
